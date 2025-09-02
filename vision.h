732b81c3f37115eb3bf24df9f87676770ea18329
pskotak <eltec@eltecweb.cz>
2025-09-02 20:02:40 +0200

    chore: Delete commented code

--- vision.h
+++ vision.h
@@ -10,10 +10,6 @@
 
 namespace vision {
 
-// #define D455W 848
-// #define D455H 480
-//
-// #define D455depth_color_max 3000.0f
 #define D455FrameRate 30
 
 extern cv::Mat depth_image16;
@@ -24,13 +20,6 @@ extern std::atomic<bool> NewD455;
 extern uint32_t StartPcRow;
 extern uint32_t EndPcRow;
 extern uint32_t IgnoreFromLeft;
-// typedef struct {
-//     float x,y,z;
-// } T3Dpoint;
-//
-// typedef struct {
-//     float x,y,z,dist;
-// } TScanPoint;
 
 extern std::vector<T3Dpoint> PointCloud;
 extern std::vector<TScanPoint> ScPoints;
